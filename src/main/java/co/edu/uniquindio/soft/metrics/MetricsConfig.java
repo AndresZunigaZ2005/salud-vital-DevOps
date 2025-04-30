c4d271a193261de71b3f89a7727f2b90e0dc15ea
AndresZunigaZ2005 <andresf.zunigaz@uqvirtual.edu.co>
2025-04-30 08:25:31 -0500

    cambio nuevo

--- src/main/java/co/edu/uniquindio/soft/metrics/MetricsConfig.java
+++ src/main/java/co/edu/uniquindio/soft/metrics/MetricsConfig.java
@@ -3,6 +3,11 @@ package co.edu.uniquindio.soft.metrics;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 
+/*
+
+
+*/
+
 public class MetricsConfig {
 
     private static final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
